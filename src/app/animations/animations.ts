f9b9bda690f41b625f6ead239a689f998e07c788
carlos <carlosramos@sime.es>
2021-12-28 18:14:17 +0100

    add comment to animation fade

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,5 +1,8 @@
 import { animate, animation, style } from "@angular/animations";
 
+/**
+ * Animation to do fadeIn or fadeOut
+ */
 export const fadeAnimation = animation([
     style({ opacity: '{{ startOpacity }}' }),
     animate('{{ time }}', style({ opacity: '{{ endOpacity }}'}))
